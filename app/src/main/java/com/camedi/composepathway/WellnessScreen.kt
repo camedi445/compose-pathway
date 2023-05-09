9ce7481681d3d74c0f37406aa11d0d866f9dfccf
Camilo Medina <camedi445@gmail.com>
2023-05-08 20:18:15 -0500

    Add wellness task feature

---
+++ app/src/main/java/com/camedi/composepathway/WellnessScreen.kt
@@ -0,0 +1,43 @@
+package com.camedi.composepathway
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.camedi.composepathway.ui.theme.ComposePathwayTheme
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task ->
+                wellnessViewModel.remove(task)
+            }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WellnessScreenPreview() {
+    ComposePathwayTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            WellnessScreen()
+        }
+    }
+}
